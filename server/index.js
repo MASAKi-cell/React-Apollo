42ad64b1772fd0d57d628ce294ac01883235c81d
MASAKi-cell <bje35149@gmail.com>
2022-02-19 17:06:55 +0900

    launchAPIとuserAPIをresolverのcontextに追加する処理を実装

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const typeDefs = require('./schema/schema');
 const { createStore } = require('./utils');
 const { LaunchAPI } = require('./datasources/launch');
 const { UserAPI } = require('./datasources/user');
+const resolvers = require('./resolvers');
 
 const store = createStore();
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers,
     dataSources: () => ({
+        // resolverのcontext引数を通してUserAPIとLaunchAPIにアクセス可能
         launchAPI: new LaunchAPI(),
         userAPI: new UserAPI({ store })
     })
