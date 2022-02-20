ad0d1e0dae0eed1969594a70ae7aa5f9d8d710e5
MASAKi-cell <bje35149@gmail.com>
2022-02-20 17:30:51 +0900

    paginating するための機能wo

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,20 @@
 module.exports = {
-    // launches、launch、meのリゾルバーを定義
+  // launches、launch、meのリゾルバーを定義
   Query: {
-    launches: (_, __, { dataSources }) =>
-      dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches,
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor : false 
+      }
+    },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
