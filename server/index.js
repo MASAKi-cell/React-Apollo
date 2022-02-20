ccc9a1776e78bfa8d49c0c02b202dbcb75ffb255
MASAKi-cell <bje35149@gmail.com>
2022-02-20 17:43:15 +0900

    モジュールの読み込み修正

--- server/index.js
+++ server/index.js
@@ -1,22 +1,21 @@
-const { ApolloServer, ApolloError } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema/schema');
 const { createStore } = require('./utils');
-const { LaunchAPI } = require('./datasources/launch');
-const { UserAPI } = require('./datasources/user');
 const resolvers = require('./resolvers');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
 const store = createStore();
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => ({
-        // resolverのcontext引数を通してUserAPIとLaunchAPIにアクセス可能
-        launchAPI: new LaunchAPI(),
-        userAPI: new UserAPI({ store })
-    })
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
 });
 
 server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-})
+  console.log(`🚀 Server ready at ${url}`);
+});
